f5d7a114bdc08427bd6dadcb519524c18390edd0
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-03-10 20:43:12 +0800

    feat: EventCard test

---
+++ src/components/EventCars.spec.js
@@ -0,0 +1,24 @@
+import { mount } from '@vue/test-utils'
+import EventCard from './EventCard.vue'
+
+describe('EventCard', () => {
+  it("renders the event's data successfully", () => {
+    const event = {
+      id: 1,
+      time: '12:00PM',
+      date: 'September 29th, 2022',
+      title: 'Coaching Little League'
+    }
+
+    const wrapper = mount(EventCard, {
+      propsData: {
+        event
+      }
+    })
+
+    const wrapperHtml = wrapper.html()
+    expect(wrapperHtml).toContain(event.time)
+    expect(wrapperHtml).toContain(event.date)
+    expect(wrapperHtml).toContain(event.title)
+  })
+})
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,12 @@
 <template>
   <h1>Events for Good</h1>
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <router-link
+      class="event-link"
+      :to="{ name: 'EventDetails', params: { id: event.id } }"
+    >
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </router-link>
   </div>
 </template>
 
